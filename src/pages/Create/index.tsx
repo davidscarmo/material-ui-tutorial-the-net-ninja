6399397b8a0b059ea6aef3fe5644c0ae22a87b24
David Carmo <davidsc2812@gmail.com>
2021-12-04 21:53:28 -0300

    material-ui forms components

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { FormControlLabel, makeStyles } from "@material-ui/core";
+
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
-import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
-const useStyles = makeStyles({});
+export const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
 
 export default function Create() {
-  // const classes = useStyles();
+  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsErrors] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsErrors(false);
+
+    if (title == "") {
+      setTitleError(true);
+    }
+    if (details == "") {
+      setDetailsErrors(true);
+    }
+
+    if (title && details) {
+      console.log(title, details, category);
+    }
+  };
   return (
     <>
       {" "}
@@ -21,16 +56,62 @@ export default function Create() {
         >
           Create a new note
         </Typography>
-
-        <Button
-          type="submit"
-          color="secondary"
-          variant="contained"
-          onClick={() => console.log("You clicked me")}
-          endIcon={<KeyboardArrowRightIcon />}
-        >
-          Submit
-        </Button>
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField
+            onChange={(e) => setTitle(e.target.value)}
+            className={classes.field}
+            label="Note Title"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+            error={titleError}
+          />
+          <TextField
+            onChange={(e) => setDetails(e.target.value)}
+            className={classes.field}
+            label="Details"
+            variant="outlined"
+            multiline
+            rows={4}
+            color="secondary"
+            fullWidth
+            required
+            error={detailsError}
+          />
+          <FormControl className={classes.field}>
+            <FormLabel>Note Category </FormLabel>
+            <RadioGroup
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <FormControlLabel
+                value="money"
+                control={<Radio />}
+                label="Money"
+              />
+              <FormControlLabel
+                value="todos"
+                control={<Radio />}
+                label="Todos"
+              />
+              <FormControlLabel
+                value="reminders"
+                control={<Radio />}
+                label="Reminders"
+              />
+              <FormControlLabel value="work" control={<Radio />} label="Work" />
+            </RadioGroup>
+          </FormControl>
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+          >
+            Submit
+          </Button>
+        </form>
         {/* Icons
         <br />
         <AcUnitOutlinedIcon />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import { ServerStyleSheets } from "@material-ui/styles";
 
 export default class MyDocument extends Document {
   render() {
@@ -12,7 +14,7 @@ export default class MyDocument extends Document {
           />
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
         </Head>
-        <body> 
+        <body>
           <Main />
           <NextScript />
         </body>
@@ -20,3 +22,25 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
